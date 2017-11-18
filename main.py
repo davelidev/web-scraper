a394c7f9b1c2c1b2b103122a8630f9c05d615520
daveli <dave322942780>
2017-11-18 16:46:08 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
+
+# -*- coding: utf-8 -*-
+
+from scrapy.cmdline import execute
+import os
+
+
+if __name__ == '__main__':
+    # sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    os.chdir('web')
+    with open('../resources.txt') as f:
+        context = f.read()
+        for line in context.strip().split('\n'):
+            if not line.strip().startswith('#'):
+                line = line.split(" ")
+                try:
+                    url = folder = year = ''
+                    if len(line) == 2:
+                        url, folder = line
+                    elif len(line) == 3:
+                        url, folder, year = line
+                        year = ' -a year=' + year
+                    execute(("scrapy crawl web -a base_url=%s -a folder_name=%s%s" % (url, folder, year)).split())
+                except:
+                    pass
